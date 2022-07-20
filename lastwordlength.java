4b1fa2610956e6c142e9e4a017a9879778656497
PenguinNP <101454762+PenguinNP@users.noreply.github.com>
2022-07-20 20:17:20 +0545

    Java Codes

--- lastwordlength.java
+++ lastwordlength.java
@@ -1,25 +1,20 @@
 class wordlength{
 	 public int length (String word){
 		int len=0;
-		int space = 0;
-		for(int i = (word.length()-1);i>0;i--){
+		 boolean space = false;
+		for(int i = (word.length()-1);i>-1;i--){
 
-				if (word.charAt(i) == ' ') {
-					space += 1;
-				} else {
-					break;
+				if (Character.isLetter(word.charAt(i))){
+				 len++;
+				 space = true;
 				}
-			}
-		 for(int i = (word.length()-(space+1));i>0;i--){
-
-			 if (word.charAt(i) == ' ') {
-				 return len;
-			 } else {
-				 len += 1;
-			 }
-		 }
-
-		return 0;
+				else {
+					if (space==true){
+						return len;
+					}
+				}
+		}
+		return  len;
 
 	}
 
@@ -27,7 +22,7 @@ class wordlength{
 public class lastwordlength {
 	public static void main(String[] args) {
 		wordlength a = new wordlength();
-		String sentence ="Fly Me To The Moooooon     ";
+		String sentence ="day";
 		System.out.println(a.length(sentence));
 
 	}
