075d9ea7fd07ac56bea96983dfbc7aee71cca776
PenguinNP <101454762+PenguinNP@users.noreply.github.com>
2022-07-21 08:36:15 +0545

    Java Codes

--- sodokusolver.java
+++ sodokusolver.java
@@ -2,10 +2,38 @@ public class sodokusolver {
 	private static final int Board_Size = 9;
 
 	public static void main(String[] args) {
+		int[][] board= {
+				{7, 0, 2, 0, 5, 0, 6, 0, 0},
+				{0, 0, 0, 0, 0, 3, 0, 0, 0},
+				{1, 0, 0, 0, 0, 9, 5, 0, 0},
+				{8, 0, 0, 0, 0, 0, 0, 9, 0},
+				{0, 4, 3, 0, 0, 0, 7, 5, 0},
+				{0, 9, 0, 0, 0, 0, 0, 0, 8},
+				{0, 0, 9, 7, 0, 0, 0, 0, 5},
+				{0, 0, 0, 2, 0, 0, 0, 0, 0},
+				{0, 0, 7, 0, 4, 0, 2, 0, 3}
+		};
+		if(boardCheck(board)){
+			System.out.println("Board is SolveAble");
+		}
+		else{
+			System.out.println("Board is Invalid");
+		}
+
+		print(board);
+
+	}
+	private  static final void print(int[][] board){
+		for ( int i=0;i<Board_Size;i++){
+			for (int j=0;j<Board_Size;j++){
+				System.out.print(board[i][j]);
+			}
+			System.out.println();
+		}
 
 	}
 
-	public boolean checkRow(int[][] board, int row, int num) {
+	private static final boolean checkRow(int[][] board, int row, int num) {
 		for (int i = 0; i < Board_Size; i++) {
 			if (board[row][i] == num) {
 				return true;
@@ -14,7 +42,7 @@ public class sodokusolver {
 		return false;
 	}
 
-	public boolean checkColumn(int[][] board, int column, int num) {
+	private static final boolean checkColumn(int[][] board, int column, int num) {
 		for (int i = 0; i < Board_Size; i++) {
 			if (board[i][column] == num) {
 				return true;
@@ -23,7 +51,7 @@ public class sodokusolver {
 		return false;
 	}
 
-	public boolean checkbox(int[][] board, int row, int column, int num) {
+	private static final boolean checkbox(int[][] board, int row, int column, int num) {
 		int Row = row - row % 3;
 		int Column = column - column % 3;
 		for (int i = Row; i < Row + 3; i++) {
@@ -36,11 +64,11 @@ public class sodokusolver {
 		return false;
 	}
 
-	boolean isValid(int[][] board, int row, int column, int num) {
+	private static final boolean isValid(int[][] board, int row, int column, int num) {
 		return !checkRow(board, row, num) && !checkColumn(board, column, num) && !checkbox(board, row, column, num);
 	}
 
-	boolean boardCheck(int[][] board) {
+	private static final boolean boardCheck(int[][] board) {
 		for (int row = 0; row < Board_Size; row++) {
 			for (int column = 0; column < Board_Size; column++) {
 				if (board[row][column] == 0) {
